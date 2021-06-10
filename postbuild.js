afbd6a26e9b9142e4988b295784b36f26ed75966
Jason Xu <Xinwen31415@gmail.com>
2021-06-10 14:10:35 -0500

    added noop callback

--- postbuild.js
+++ postbuild.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
 fs.writeFileSync('./.env', 'MONGODB_URL=mongodb+srv://admin:mfWdh6eHtZvqMqh6@gemini-main.kutxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-fs.rm('./postbuild.js');
+fs.rm('./postbuild.js', () => {});
