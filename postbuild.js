a5dc0d4521cb54d14a4efdc8c1a3ca972c56758d
Jason Xu <Xinwen31415@gmail.com>
2021-06-10 14:14:32 -0500

    removed postbuild

--- postbuild.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync('./.env', 'MONGODB_URL=mongodb+srv://admin:mfWdh6eHtZvqMqh6@gemini-main.kutxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-fs.rm('./postbuild.js', () => {});
