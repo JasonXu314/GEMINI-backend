1a36ce890ac2752ddee6960bbfbea8fbc045e96a
Jason Xu <Xinwen31415@gmail.com>
2021-07-15 17:39:35 -0500

    added logging message

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
 
 	app.enableCors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'https://gemini-castor.vercel.app'], credentials: true });
 
+	console.log(`Binding to ${process.env.PORT || 5000}...`);
 	await app.listen(process.env.PORT || 5000);
 }
 
